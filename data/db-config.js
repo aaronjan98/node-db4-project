e7dd980b02c3dee625f173e639f48b1b997d11a5
aaronjan <aaronjan98@gmail.com>
2020-02-20 15:28:19 -0800

    ran migrate:latest to create a recipes table

---
+++ data/db-config.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+
+const config = {
+    client = 'sqlite3',
+    connection: {
+        filename: './data/file/recipes.db3'
+    },
+    useNullAsDefault = true
+};
+
+const db = knex(config);
+
+module.exports = db;
